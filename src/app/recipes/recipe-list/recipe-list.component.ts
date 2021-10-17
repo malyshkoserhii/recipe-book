030ad604c014a3f7020f1d7bfe530a20bfd30f63
Serhii Malyshko <serhiimalyshko@Serhiis-Mac-mini.local>
2021-10-17 19:21:52 +0300

    feat: Markup of main components added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simple a test',
+      'https://img.taste.com.au/31kwj5qu/w643-h428-cfill-q90/taste/2018/10/slow-cooker-pork-belly-in-apricot-sauce-recipe-143088-2.jpg',
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
